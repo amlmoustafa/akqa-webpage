a7dd8b25c947459b3ef342ca4ec2909c87ae7ffc
amal@devsquads.com <amal@devsquads.com>
2021-08-13 20:03:37 +0200

    finished reference section

--- src/components/References.js
+++ src/components/References.js
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Container,
   Grid,
@@ -11,14 +12,40 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 const References = () => {
   const classes = useStyles();
   return (
-    <Container>
-      <Button size="small">
-        <ExpandMoreIcon />
-      </Button>
-    </Container>
+    <Box className={classes.container}>
+      <Container className={classes.contentContainer}>
+        <div className={classes.referenceBtn}>
+          <ExpandMoreIcon />
+        </div>
+        <Typography variant="h6" className={classes.reference}>
+          References
+        </Typography>
+      </Container>
+    </Box>
   );
 };
 
-const useStyles = makeStyles(() => ({}));
+const useStyles = makeStyles(() => ({
+  container: { padding: "50px 0", backgroundColor: "#E6E7E8" },
+  contentContainer: { overflow: "hidden" },
+  referenceBtn: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
+    backgroundColor: "#00609C",
+    color: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    float: "left",
+    marginRight: 10,
+    cursor: "pointer",
+  },
+  reference: {
+    color: "#00609C",
+    fontSize: 24,
+    float: "left",
+  },
+}));
 
 export default References;
